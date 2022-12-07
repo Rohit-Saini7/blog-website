7fd249c46f0c1d17b838f0f73c12020537f3839a
Rohit-Saini7 <rohit712wd@gmail.com>
2022-12-07 16:07:33 +0530

    fourth

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,10 @@ function Footer() {
         <Link href='https://nextjs.org/'> Next.JS </Link>
         and<Link href='https://hygraph.com/'> GraphCMS </Link> powered Blog made
         with ❤️ by
-        <Link href='https://rohitsaini.tech/'> Rohit</Link>
+        <Link href='https://www.fiverr.com/rohit_saini_7/get-your-website-built-reactjs'>
+          {' '}
+          Rohit
+        </Link>
       </p>
     </footer>
   );
